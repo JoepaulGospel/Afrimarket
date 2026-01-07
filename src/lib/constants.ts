5d57e0fc09b12df07de83a366f4fff4043c839cb
JoepaulGospel <162193500+JoepaulGospel@users.noreply.github.com>
2026-01-07 05:47:13 +0100

    MERK Update: src/lib/constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,20 +1,8 @@
 export const AFRIMARKET_CONFIG = {
-FX_RATE: 1650, // Update this as the Naira fluctuates
+FX_RATE: 1650,
 SERVICE_FEE_PERCENT: 0.05,
-SHIPPING_RATES: {
-US: 12, // $12/kg
-UK: 10, // $10/kg
-CA: 11, // $11/kg
-CN: 9 // $9/kg
-},
-DUTY_RATES: {
-GADGETS: 0.20,
-CLOTHING: 0.10,
-GENERAL: 0.15
-},
-LOCAL_DELIVERY: {
-LAGOS: 3500,
-OUTSIDE_LAGOS: 6500
-},
-RESTRICTED_KEYWORDS: ["battery", "lithium", "liquid", "perfume", "powerbank", "flammable"]
+SHIPPING_RATES: { US: 12, UK: 10, CA: 11, CN: 9 },
+DUTY_RATES: { GADGETS: 0.20, CLOTHING: 0.10, GENERAL: 0.15 },
+LOCAL_DELIVERY: { LAGOS: 3500, OUTSIDE_LAGOS: 6500 },
+RESTRICTED_KEYWORDS: ["battery", "lithium", "liquid", "perfume", "powerbank", "flammable", "oil", "chemical"]
 };
